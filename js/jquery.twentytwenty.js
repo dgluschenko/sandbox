17a3f0689599bb5a94fca1e50594ac249ae88a20
dgluschenko <109702546+dgluschenko@users.noreply.github.com>
2022-07-21 22:06:05 +0200

    Update jquery.twentytwenty.js

--- js/jquery.twentytwenty.js
+++ js/jquery.twentytwenty.js
@@ -4,10 +4,7 @@
     var options = $.extend({
       default_offset_pct: 0.5,
       orientation: 'horizontal',
-      before_label: 'Before',
-      after_label: 'After',
       no_overlay: false,
-      move_slider_on_hover: false,
       move_with_handle_only: true,
       click_to_move: false
     }, options);
